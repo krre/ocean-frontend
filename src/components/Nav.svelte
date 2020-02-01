1d2ea18e56feff6d5fc70f6e60d4e9600a1db310
Vladimir Zarypov <krre31@gmail.com>
2020-02-01 13:45:13 +0200

    Add navigation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,5 +7,9 @@
 </style>
 
 <nav>
-
+	<ul>
+		<li><a href=".">Каталог</a></li>
+		<li><a href="append">Добавить</a></li>
+		<li><a href="about">О сайте</a></li>
+	</ul>
 </nav>
---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>О сайте</title>
+</svelte:head>
+
+<h1>О сайте</h1>
+
+Скоро всё будет...
