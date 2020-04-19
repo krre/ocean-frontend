704b91ef594752fbe5516b62a59f84b466c0d3b9
Vladimir Zarypov <krre31@gmail.com>
2020-04-19 15:40:15 +0300

    Add 'login' page

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,9 +7,18 @@
 </style>
 
 <nav>
-	<ul>
-		<li><a href=".">Каталог</a></li>
-		<li><a href="append">Добавить</a></li>
-		<li><a href="about">О сайте</a></li>
-	</ul>
+    <ul>
+        <li>
+            <a href=".">Каталог</a>
+        </li>
+        <li>
+            <a href="append">Добавить</a>
+        </li>
+        <li>
+            <a href="about">О сайте</a>
+        </li>
+        <li>
+            <a href="login">Войти</a>
+        </li>
+    </ul>
 </nav>
---
+++ src/routes/login.svelte
@@ -0,0 +1,44 @@
+<script>
+    let id = "";
+    let password = "";
+    let save = true;
+
+    async function login() {
+        if (!id) {
+            alert("Идентификатор не заполнен!");
+            return;
+        }
+
+        if (!password) {
+            alert("Пароль не заполнен!");
+            return;
+        }
+
+        console.log("login", id, password, save);
+    }
+</script>
+
+<style>
+    .form {
+        display: grid;
+        grid-gap: 10px;
+        width: 200px;
+    }
+</style>
+
+<svelte:head>
+    <title>Войти</title>
+</svelte:head>
+
+<div class="form">
+    Идентификатор:
+    <input type="number" bind:value={id} />
+    Пароль:
+    <input type="password" bind:value={password} />
+    <label>
+        Запомнить
+        <input type="checkbox" checked="true" bind:checked={save} />
+    </label>
+    <button on:click={login}>Войти</button>
+    <button>Регистрация</button>
+</div>
