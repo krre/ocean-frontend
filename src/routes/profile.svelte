c44c5dd66bafb5f654116ae111d45b3fb57fcb63
Vladimir Zarypov <krre31@gmail.com>
2020-05-21 19:56:21 +0300

    Get name for 'Profile' page

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,6 +1,42 @@
+<script context="module">
+    import { Request } from "../js/json-rpc.js";
+    import { sendRequest } from "../js/net.js";
+
+    export async function preload(page, session) {
+        let users = await sendRequest(
+            new Request("user.get", { token: session.user.token })
+        );
+
+        const user = users[0];
+        console.log(user);
+
+        return { user };
+    }
+</script>
+
+<script>
+    import { stores } from "@sapper/app";
+
+    const { session } = stores();
+
+    export let user;
+    $: name = user.name;
+</script>
+
+<style>
+    .form {
+        display: grid;
+        grid-gap: 10px;
+        width: 200px;
+    }
+</style>
+
 <svelte:head>
     <title>Профиль</title>
 </svelte:head>
 
 <h1>Профиль</h1>
-Скоро всё будет...
+<div class="form">
+    Имя:
+    <input bind:value={name} />
+</div>
