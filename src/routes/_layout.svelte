28325105e136e597a274ee58b0ca1a5167f3bc56
Vladimir Zarypov <krre31@gmail.com>
2020-05-28 20:03:10 +0300

    Add warning header

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,9 +5,19 @@
 </script>
 
 <style>
-
+    p {
+        color: white;
+        background-color: red;
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+    }
 </style>
 
+<p>
+    Сайт находится в разработке! Возможны перебои в работе и полное удаление
+    всех данных!
+</p>
 <Nav {segment} />
 
 <main>
