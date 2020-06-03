9944a36918352dd49d8f84c636c5d984727fda4d
Vladimir Zarypov <krre31@gmail.com>
2020-06-03 08:25:51 +0300

    Add page layout

---
+++ src/routes/page/[page].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    export function preload({ params }) {
+        return {
+            currentPage: +params.page
+        };
+    }
+</script>
+
+<script>
+    import Home from "../../components/Home.svelte";
+    export let currentPage;
+</script>
+
+<Home {currentPage} />
