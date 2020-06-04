2b89ff858e8a446873962e544b9329a988b429c3
Vladimir Zarypov <krre31@gmail.com>
2020-06-04 08:54:44 +0300

    Remove develop advert

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,20 +4,6 @@
     export let segment;
 </script>
 
-<style>
-    p {
-        color: white;
-        background-color: red;
-        height: 50px;
-        text-align: center;
-        line-height: 50px;
-    }
-</style>
-
-<p>
-    Сайт находится в разработке! Возможны перебои в работе и полное удаление
-    всех данных!
-</p>
 <Nav {segment} />
 
 <main>
