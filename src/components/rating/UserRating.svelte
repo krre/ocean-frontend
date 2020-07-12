3263d310296ece56f95067a8deed3b05f0feb373
Vladimir Zarypov <krre31@gmail.com>
2020-07-12 15:17:05 +0300

    Add user rating

--- src/components/rating/UserRating.svelte
+++ src/components/rating/UserRating.svelte
@@ -1,9 +1,23 @@
 <script>
     import { onMount } from "svelte";
+    import { send } from "net.js";
+    import * as consts from "consts.js";
 
     onMount(async () => {
-        console.log("user rating");
+        load();
     });
+
+    let users = [];
+
+    async function load() {
+        users = await send("rating.getUsers");
+    }
 </script>
 
-Доска почёта
+<p>Количество мандел, добавленных пользователями.</p>
+
+{#each users as user, i}
+    {i + 1}. {user.name || consts.AccountModeNames[consts.ConspiratorAccount]} -
+    {user.count}
+    <br />
+{/each}
