68fb81b67e4589adb77022ad4105da223bdf1377
Vladimir Zarypov <krre31@gmail.com>
2020-10-14 07:41:23 +0300

    Rating vote setted by URL query

--- src/components/rating/MandelaRating.svelte
+++ src/components/rating/MandelaRating.svelte
@@ -1,16 +1,44 @@
 <script lang="ts">
     import * as consts from "consts";
-    import { stores } from "@sapper/app";
+    import { goto, stores } from "@sapper/app";
     import { send } from "network";
     import { makeTitle } from "utils";
 
-    const { session } = stores();
+    const { page } = stores();
 
     let mandels = [];
     let vote = 0;
 
-    $: if (process.browser && vote >= 0) {
+    class NonReactive {
+        pageInit = false;
+
+        setPageInit() {
+            this.pageInit = true;
+        }
+    }
+
+    const nonReactive = new NonReactive();
+
+    $: if (nonReactive.pageInit && vote >= 0) {
+        const params = new URLSearchParams();
+
+        if (vote) {
+            params.append("vote", vote.toString());
+        }
+
+        const query = params.toString();
+        const resQuery = consts.Route.Rating + (query ? "?" + query : "");
+        goto(resQuery);
+    }
+
+    $: if (process.browser && $page.query) {
+        assignQuery();
         load();
+        nonReactive.setPageInit();
+    }
+
+    function assignQuery() {
+        vote = +$page.query.vote || 0;
     }
 
     async function load() {
