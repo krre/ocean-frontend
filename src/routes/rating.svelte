33d540ad466bd87ada522db965ba719f7787cef9
Vladimir Zarypov <krre31@gmail.com>
2020-10-14 06:19:51 +0300

    Rating type setted by URL query

--- src/routes/rating.svelte
+++ src/routes/rating.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
     import MandelaRating from "../components/rating/MandelaRating.svelte";
     import UserRating from "../components/rating/UserRating.svelte";
+    import { goto, stores } from "@sapper/app";
 
+    const { page } = stores();
     let type = 0;
+
+    class NonReactive {
+        pageInit = false;
+
+        setPageInit() {
+            this.pageInit = true;
+        }
+    }
+
+    const nonReactive = new NonReactive();
+
+    $: if (nonReactive.pageInit && type >= 0) {
+        const params = new URLSearchParams();
+
+        if (type) {
+            params.append("type", type.toString());
+        }
+
+        const query = params.toString();
+        const resQuery = "rating" + (query ? "?" + query : "");
+        goto(resQuery);
+    }
+
+    $: if (process.browser && $page.query) {
+        assignQuery();
+        nonReactive.setPageInit();
+    }
+
+    function assignQuery() {
+        type = +$page.query.type || 0;
+    }
 </script>
 
 <svelte:head>
