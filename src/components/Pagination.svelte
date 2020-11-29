85f9aa637d043bf7733ddd8338260839a82aeef0
Vladimir Zarypov <krre31@gmail.com>
2020-11-29 20:11:36 +0200

    Remake pagination

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -2,12 +2,36 @@
     export let baseRoute = "";
     export let baseQuery = new URLSearchParams();
     export let pageQuery = new URLSearchParams();
-    export let limit = 0;
-    export let count = 0;
+    export let limit = 1;
+    export let count = 1;
     export let offset = 1;
+    let pages = [];
 
     $: last = Math.ceil(count / limit);
     $: pageQuery = query(offset);
+    $: {
+        const maxPageSelectors = 5;
+        pages = Array(Math.min(last, maxPageSelectors));
+
+        pages[0] = 1;
+        pages[pages.length - 1] = last;
+
+        let start = 2;
+
+        if (pages.length == maxPageSelectors && offset >= 4) {
+            if (last - offset <= 3 && offset > last - 3) {
+                start = last - 3;
+            } else if (offset >= 4) {
+                start = offset - 1;
+            }
+        }
+
+        for (let i = 0; i < pages.length - 2; i++) {
+            pages[i + 1] = start + i;
+        }
+
+        pages = pages;
+    }
 
     function query(page: number, qry?: URLSearchParams): URLSearchParams {
         let result = new URLSearchParams(qry);
@@ -27,42 +51,35 @@
 </script>
 
 <style>
-    .pagination-container {
+    .container {
         display: flex;
         justify-content: center;
         align-items: center;
-        margin: 0.5em 0;
+        text-align: center;
     }
 
-    .pagination-item {
-        padding: 0 5px;
-        text-decoration: none;
-        color: rgb(51, 51, 51);
+    span {
+        width: 2em;
+        padding: 0.7em;
+        border-top: var(--border-1px);
+        border-bottom: var(--border-1px);
+        border-left: var(--border-1px);
     }
 
-    .pagination-item:hover {
-        text-decoration: underline;
+    span:last-child {
+        border-right: var(--border-1px);
+    }
+
+    .offset {
+        background-color: powderblue;
     }
 </style>
 
 {#if count > limit}
-    <div class="pagination-container">
-        {#if offset > 1}
-            <a class="pagination-item" href={makeLink(1, baseQuery)}>Первая</a>
-            <a
-                class="pagination-item"
-                href={makeLink(offset - 1, baseQuery)}>Предыдущая</a>
-        {/if}
-
-        <div class="pagination-item">{offset}</div>
-
-        {#if offset < last}
-            <a
-                class="pagination-item"
-                href={makeLink(offset + 1, baseQuery)}>Следующая</a>
-            <a
-                class="pagination-item"
-                href={makeLink(last, baseQuery)}>Последняя</a>
-        {/if}
+    <div class="container">
+        {#each pages as p}
+            <span class:offset={offset == p}><a
+                    href={makeLink(p, baseQuery)}>{p}</a></span>
+        {/each}
     </div>
 {/if}
