d3f7ef04c811456df3a1933bd3c491e5b9d342f7
Vladimir Zarypov <krre31@gmail.com>
2020-12-14 19:58:49 +0200

    Add BBCode unit tests

---
+++ tests/bbcode.spec.ts
@@ -0,0 +1,10 @@
+export { }
+import { parse } from "../src/node_modules/bbcode"
+
+test("BBCode bold", () => {
+    expect(parse("[b]bold text[/b]")).toBe("<strong>bold text</strong>");
+})
+
+test("BBCode raw link", () => {
+    expect(parse("[url]http://ocean-mandela.info[/url]")).toBe(`<a href="http://ocean-mandela.info">http://ocean-mandela.info</a>`);
+});
