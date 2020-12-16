5acb78f0cba4f8d4c2dcbebba69bdf5d372121ff
Vladimir Zarypov <krre31@gmail.com>
2020-12-16 07:50:37 +0200

    Add 'attr' pattern to parse BBCode

--- tests/bbcode/parser.spec.ts
+++ tests/bbcode/parser.spec.ts
@@ -1,12 +1,16 @@
 export { }
-import { parse } from "../../src/node_modules/bbcode"
+import { parse, attr } from "../../src/node_modules/bbcode/parser"
 
 describe("BBCode parser", () => {
-    test("BBCode bold", () => {
-        expect(parse("[b]bold text[/b]")).toBe("<strong>bold text</strong>");
-    })
-
-    test("BBCode raw link", () => {
-        expect(parse("[url]http://ocean-mandela.info[/url]")).toBe(`<a href="http://ocean-mandela.info">http://ocean-mandela.info</a>`);
+    test("attr", () => {
+        expect(attr.exec('width="500"', 0)).toStrictEqual({ res: { name: "width", value: "500" }, end: 11 })
     });
+
+    // test("bold", () => {
+    //     expect(parse("[b]bold text[/b]")).toBe("<strong>bold text</strong>");
+    // })
+
+    // test("raw link", () => {
+    //     expect(parse("[url]http://ocean-mandela.info[/url]")).toBe(`<a href="http://ocean-mandela.info">http://ocean-mandela.info</a>`);
+    // });
 })
