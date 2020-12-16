d7c78c1664e31eb5787c87835202b501cf1ea9fe
Vladimir Zarypov <krre31@gmail.com>
2020-12-16 19:17:32 +0200

    Add unit tests for simple bbcode patterns

--- tests/bbcode/parser.spec.ts
+++ tests/bbcode/parser.spec.ts
@@ -1,10 +1,38 @@
 export { }
-import { parse, attr } from "../../src/node_modules/bbcode/parser"
+import { name, char, wsp, text, quoted, attr, attrs, node } from "../../src/node_modules/bbcode/parser"
 
 describe("BBCode parser", () => {
+    test("name", () => {
+        expect(name.exec("Width", 0)).toStrictEqual({ res: "width", end: 5 })
+    });
+
+    test("char", () => {
+        expect(char.exec("x", 0)).toStrictEqual({ res: "x", end: 1 })
+    });
+
+    test("wsp", () => {
+        expect(wsp.exec("  ", 0)).toStrictEqual({ res: "  ", end: 2 })
+    });
+
+    test("text", () => {
+        expect(text.exec("w3$u", 0)).toStrictEqual({ res: "w3$u", end: 4 })
+    });
+
+    test("quoted", () => {
+        expect(quoted.exec('"400"', 0)).toStrictEqual({ res: "400", end: 5 })
+    });
+
     test("attr", () => {
         expect(attr.exec('width="500"', 0)).toStrictEqual({ res: { name: "width", value: "500" }, end: 11 })
     });
+
+    test("attrs", () => {
+        expect(attrs.exec('width="500" height="200"', 0)).toStrictEqual({ res: { width: "500", height: "200" }, end: 24 })
+    });
+
+    // test("node", () => {
+    //     expect(node.exec('[b]bold text[/b]', 0)).toStrictEqual({ res: { name: "b", attrs: {} }, end: 11 })
+    // });
 
     // test("bold", () => {
     //     expect(parse("[b]bold text[/b]")).toBe("<strong>bold text</strong>");
