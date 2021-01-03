baefb4ebe9e5d4b6b2879adf09322b2e00bf2c4d
Vladimir Zarypov <krre31@gmail.com>
2021-01-03 16:16:03 +0200

    Add 'link' property to Latest component

--- src/components/main/activity/Latest.svelte
+++ src/components/main/activity/Latest.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import Rectangle from "../../Rectangle.svelte";
     export let title = "";
+    export let link = "#";
 </script>
 
 <style>
+    .title {
+        margin-bottom: var(--page-margin);
+    }
 </style>
 
 <Rectangle>
-    <div>{title}</div>
+    <div class="title"><a href={link}>{title}</a></div>
     <slot />
 </Rectangle>
--- src/components/main/activity/LatestForumPosts.svelte
+++ src/components/main/activity/LatestForumPosts.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import * as route from "route";
     import Latest from "./Latest.svelte";
 </script>
 
 <style>
 </style>
 
-<Latest title="Последние сообщения форума">Список</Latest>
+<Latest title="Последние сообщения форума" link={route.Forum.New}>
+    Список
+</Latest>
