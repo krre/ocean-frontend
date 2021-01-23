3ed637047362723e52feac5916b1d0dbd6fddbca
Vladimir Zarypov <krre31@gmail.com>
2021-01-23 10:04:37 +0200

    Add poll parameters to 'forum.topic.create' request

--- src/routes/forum/section/[id]/topic/append.svelte
+++ src/routes/forum/section/[id]/topic/append.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import * as route from "route";
     import * as api from "api";
+    import * as types from "types";
     import { goto, stores } from "@sapper/app";
     import Frame from "../../../../../components/Frame.svelte";
     import SessionHub from "../../../../../components/SessionHub.svelte";
@@ -13,11 +14,21 @@
     let isAdmin = false;
     let name: string;
 
+    let type = types.ForumTopicType.Common;
+    let answerSelection = types.ForumPollAnswerSelection.One;
+    let answers: string[] = [];
+
     const action = async () => {
         const params: api.Forum.Topic.Create.Request = {
             section_id: sectionId,
             name: name,
+            type: type,
         };
+
+        if (type === types.ForumTopicType.Poll) {
+            params.poll_answers = answers;
+            params.poll_answer_selection = answerSelection;
+        }
 
         await api.Forum.Topic.Create.exec(params);
         goto(route.Forum.Section.Id(sectionId));
@@ -27,5 +38,11 @@
 <SessionHub bind:isAdmin />
 
 <Frame {title}>
-    <TopicEditor bind:name {action} />
+    <TopicEditor
+        bind:name
+        bind:type
+        bind:answerSelection
+        bind:answers
+        {action}
+    />
 </Frame>
