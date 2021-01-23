1cf7d3ba0fb32e083cd0378abb11647ae9c7591f
Vladimir Zarypov <krre31@gmail.com>
2021-01-23 09:24:21 +0200

    Add poll to TopicEditor

--- src/components/forum/topic/TopicEditor.svelte
+++ src/components/forum/topic/TopicEditor.svelte
@@ -1,20 +1,115 @@
 <script lang="ts">
-    export let name: string;
-    export let action = () => {};
+    import * as types from "types";
 
-    $: buttonEnabled = name;
+    export let name = "";
+    export let action = () => {};
+    export let type = types.ForumTopicType.Common;
+    export let answerSelection = types.ForumPollAnswerSelection.One;
+    export let answers: string[] = [];
+
+    $: buttonEnabled =
+        type === types.ForumTopicType.Common ? name : name && answers.length;
+
+    function beforeAction() {
+        if (type == types.ForumTopicType.Poll) {
+            for (let answer of answers) {
+                if (!answer) {
+                    alert("В опросе есть незаполненные ответы!");
+                    return;
+                }
+            }
+        }
+
+        action();
+    }
 </script>
 
 <style>
     .form {
         display: grid;
-        grid-gap: 0.5em;
+        grid-gap: 0.8em;
     }
 
-    button {
-        margin-top: 1em;
+    .answer {
+        width: min(50em, 100%);
+        display: grid;
+        grid-template-columns: 1fr auto;
+        gap: 0.6em;
     }
 </style>
 
-<div class="form">Название: <input type="text" bind:value={name} /></div>
-<button on:click={action} disabled={!buttonEnabled}>Отправить</button>
+<div class="form">
+    Название: <input type="text" bind:value={name} />
+    <div>
+        <label>
+            <input
+                type="radio"
+                bind:group={type}
+                value={types.ForumTopicType.Common}
+            />
+            Обычная тема
+        </label>
+
+        <label>
+            <input
+                type="radio"
+                bind:group={type}
+                value={types.ForumTopicType.Poll}
+            />
+            Опрос
+        </label>
+    </div>
+
+    {#if type == types.ForumTopicType.Poll}
+        <div>Укажите вопрос в названии темы и добавьте варианты ответа.</div>
+
+        {#each answers as answer, i}
+            <div class="answer">
+                <input bind:value={answer} />
+                <button
+                    on:click={() => {
+                        answers.splice(i, 1);
+                        answers = answers;
+                    }}>Удалить</button
+                >
+            </div>
+        {/each}
+
+        <div>
+            <button
+                on:click={() => {
+                    answers.push("");
+                    answers = answers;
+                }}>Добавить</button
+            >
+        </div>
+
+        Сколько вариантов можно выбрать?
+
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    bind:group={answerSelection}
+                    value={types.ForumPollAnswerSelection.One}
+                />
+                Один
+            </label>
+
+            <label>
+                <input
+                    type="radio"
+                    bind:group={answerSelection}
+                    value={types.ForumPollAnswerSelection.Several}
+                />
+                Несколько
+            </label>
+        </div>
+    {/if}
+
+    <div>
+        <button on:click={beforeAction} disabled={!buttonEnabled}
+            >Отправить</button
+        >
+    </div>
+</div>
