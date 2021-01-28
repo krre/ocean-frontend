6a9ccf5af1aed2a49751891fe3126ffd95796974
Vladimir Zarypov <krre31@gmail.com>
2021-01-28 08:21:39 +0200

    Improve loading forum topics

--- src/routes/forum/section/[id]/index.svelte
+++ src/routes/forum/section/[id]/index.svelte
@@ -1,56 +1,70 @@
+<script context="module" lang="ts">
+    import * as api from "api";
+    import type { Session, Page } from "types";
+
+    const PAGE_LIMIT = 20;
+
+    export async function preload(page: Page, _session: Session) {
+        const sectionId = +page.params.id;
+        const pageNo = +page.query.page || 1;
+        const getAllResponse = await load(sectionId, pageNo);
+        return { getAllResponse, pageNo, sectionId };
+    }
+
+    async function load(
+        sectionId: number,
+        pageNo: number
+    ): Promise<api.Forum.Topic.GetAll.Response> {
+        const params: api.Forum.Topic.GetAll.Request = {
+            section_id: sectionId,
+            offset: (pageNo - 1) * PAGE_LIMIT,
+            limit: PAGE_LIMIT,
+        };
+
+        return api.Forum.Topic.GetAll.exec(params);
+    }
+</script>
+
 <script lang="ts">
     import * as route from "route";
-    import * as api from "api";
-    import type { PathPart } from "forum";
     import type { User } from "types";
-    import { goto, stores } from "@sapper/app";
+    import { goto } from "@sapper/app";
+    import type { PathPart } from "forum";
     import FramePage from "../../../../components/forum/main/ForumFrame.svelte";
     import SessionHub from "../../../../components/SessionHub.svelte";
     import TopicElement from "../../../../components/forum/topic/TopicElement.svelte";
     import Navigator from "../../../../components/forum/main/Navigator.svelte";
     import Pagination from "../../../../components/Pagination.svelte";
 
-    const { page } = stores();
-    const sectionId = +$page.params.id;
+    export let getAllResponse: api.Forum.Topic.GetAll.Response;
+    export let sectionId = 0;
+    export let pageNo = 0;
 
+    let topics: api.Forum.Topic.GetAll.Topic[];
+    let topicCount = 0;
     let sectionName: string;
+    let categoryNav: PathPart;
 
     let isAdmin = false;
     let user: User;
-    let topics = [];
 
-    let pageNo = 1;
-    let topicCount = 0;
-
-    let categoryNav: PathPart;
-
-    const pageLimit = 20;
-
-    $: if (process.browser && $page.query) {
-        pageNo = +$page.query.page || 1;
-        load();
-    }
-
-    async function load() {
-        const params: api.Forum.Topic.GetAll.Request = {
-            section_id: sectionId,
-            offset: (pageNo - 1) * pageLimit,
-            limit: pageLimit,
-        };
-
-        const result = await api.Forum.Topic.GetAll.exec(params);
-        sectionName = result.section_name;
-        topics = result.topics;
-        topicCount = result.topic_count;
+    $: {
+        sectionName = getAllResponse.section_name;
+        topics = getAllResponse.topics;
+        topicCount = getAllResponse.topic_count;
 
         categoryNav = {
-            id: result.category_id,
-            name: result.category_name,
+            id: getAllResponse.category_id,
+            name: getAllResponse.category_name,
         };
     }
 
     function append() {
         goto(route.Forum.Topic.Append(sectionId));
+    }
+
+    async function reload() {
+        getAllResponse = await load(sectionId, pageNo);
     }
 </script>
 
@@ -64,13 +78,13 @@
     <button slot="button" on:click={append}>Создать тему</button>
 
     {#each topics as topic}
-        <TopicElement {topic} on:removed={() => load()} />
+        <TopicElement {topic} on:removed={() => reload()} />
     {/each}
 </FramePage>
 
 <Pagination
     count={topicCount}
-    limit={pageLimit}
+    limit={PAGE_LIMIT}
     offset={pageNo}
     baseRoute={route.Forum.Section.Id(sectionId)}
 />
