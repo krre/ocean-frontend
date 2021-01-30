1101352c95d93dbc8238747338b11e9aaf958f4b
Vladimir Zarypov <krre31@gmail.com>
2021-01-30 11:42:07 +0200

    Improve loading activity

--- src/components/main/activity/Activity.svelte
+++ src/components/main/activity/Activity.svelte
@@ -1,57 +1,9 @@
 <script lang="ts">
-    import * as api from "api";
-    import * as route from "route";
-    import * as forum from "forum";
-    import { makeTitle } from "utils";
     import type { ActivityMessage } from "types";
     import Latest from "./Latest.svelte";
 
-    const LIMIT = 5;
-    let comments: ActivityMessage[] = [];
-    let topics: ActivityMessage[] = [];
-
-    $: if (process.browser) {
-        load();
-    }
-
-    async function load() {
-        const params: api.Activity.GetAll.Request = {
-            limit: LIMIT,
-        };
-        const result = await api.Activity.GetAll.exec(params);
-
-        topics = [];
-
-        result.topics.forEach((item: api.Activity.Topic) => {
-            const topic: ActivityMessage = {
-                title: item.name,
-                url: forum.topicLink(item.id, item.post_count),
-                date: item.post_create_ts,
-                author: item.user_name,
-                message: item.post,
-            };
-
-            topics.push(topic);
-        });
-
-        topics = topics;
-
-        comments = [];
-
-        result.comments.forEach((item: api.Activity.Comment) => {
-            const comment: ActivityMessage = {
-                title: makeTitle(item),
-                url: route.Mandela.Id(item.mandela_id),
-                date: item.create_ts,
-                author: item.user_name,
-                message: item.message,
-            };
-
-            comments.push(comment);
-        });
-
-        comments = comments;
-    }
+    export let comments: ActivityMessage[] = [];
+    export let topics: ActivityMessage[] = [];
 </script>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,68 @@
+<script context="module" lang="ts">
+    import * as api from "api";
+    import * as route from "route";
+    import * as forum from "forum";
+    import type { Session, Page, ActivityMessage } from "types";
+    import { makeTitle } from "utils";
+
+    const MANDELA_PAGE_LIMIT = 50;
+    const ACTIVITY_PAGE_LIMIT = 5;
+
+    export async function preload(page: Page, session: Session) {
+        const [topics, comments] = await loadActivity();
+
+        return {
+            topics,
+            comments,
+        };
+    }
+
+    async function loadActivity(): Promise<
+        [ActivityMessage[], ActivityMessage[]]
+    > {
+        const params: api.Activity.GetAll.Request = {
+            limit: ACTIVITY_PAGE_LIMIT,
+        };
+        const result = await api.Activity.GetAll.exec(params);
+
+        const topics: ActivityMessage[] = [];
+
+        result.topics.forEach((item: api.Activity.Topic) => {
+            const topic: ActivityMessage = {
+                title: item.name,
+                url: forum.topicLink(item.id, item.post_count),
+                date: item.post_create_ts,
+                author: item.user_name,
+                message: item.post,
+            };
+
+            topics.push(topic);
+        });
+
+        const comments: ActivityMessage[] = [];
+
+        result.comments.forEach((item: api.Activity.Comment) => {
+            const comment: ActivityMessage = {
+                title: makeTitle(item),
+                url: route.Mandela.Id(item.mandela_id),
+                date: item.create_ts,
+                author: item.user_name,
+                message: item.message,
+            };
+
+            comments.push(comment);
+        });
+
+        return [topics, comments];
+    }
+</script>
+
 <script lang="ts">
     import Catalog from "../components/main/Catalog.svelte";
     import Activity from "../components/main/activity/Activity.svelte";
+
+    export let topics: ActivityMessage[];
+    export let comments: ActivityMessage[];
 </script>
 
 <style>
@@ -30,5 +92,5 @@
         <Catalog />
     </div>
 
-    <Activity />
+    <Activity {topics} {comments} />
 </div>
