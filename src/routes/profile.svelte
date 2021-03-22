53fade12892b92210f5f16f328d2baccabc21eb1
Vladimir Zarypov <krre31@gmail.com>
2021-03-22 19:33:46 +0200

    Add 'gender' option to user profile

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -15,16 +15,20 @@
 </script>
 
 <script lang="ts">
+    import * as consts from "consts";
+    import * as types from "types";
     import { errorMessage } from "network";
     import type { User } from "types";
     import { stores } from "@sapper/app";
     import Frame from "../components/Frame.svelte";
     import OperationResult from "../components/OperationResult.svelte";
 
-    const { session } = stores();
-
-    const title = "Профиль";
     export let user: User;
+
+    const { session } = stores();
+    const title = "Профиль";
+
+    let currentGender = user.gender;
 
     let successProfile: string;
     let errorProfile: string;
@@ -39,6 +43,7 @@
         const params: api.User.Update.Request = {
             name: user.name,
             code: user.code,
+            gender: currentGender,
         };
 
         try {
@@ -97,11 +102,18 @@
         <div>ИД: {user.id}</div>
         <div>Создано: {formatDateTime(user.create_ts)}</div>
         Имя:<input bind:value={user.name} />
+        Пол:
+        <select bind:value={currentGender}>
+            {#each consts.Genders as gender, i}
+                <option value={i}>{gender}</option>
+            {/each}
+        </select>
         <OperationResult
             bind:success={successProfile}
             bind:error={errorProfile}
         />
         <button on:click={update} disabled={!user.name}>Сохранить</button>
+
         Пароль:
         <input type="password" bind:value={password1} />
         Пароль (ещё раз):
